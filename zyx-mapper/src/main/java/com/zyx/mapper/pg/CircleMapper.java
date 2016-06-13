573346838e9aaa6b5bdda0eaf05d318c85181b89
XiaoWei <xiaoweiqb@126.com>
2016-06-13 18:46:05 +0800

    [add] 添加circle相关dubbo 服务

---
+++ zyx-mapper/src/main/java/com/zyx/mapper/pg/CircleMapper.java
@@ -0,0 +1,10 @@
+package com.zyx.mapper.pg;
+
+import com.zyx.entity.pg.Circle;
+import com.zyx.mapper.BaseMapper;
+
+/**
+ * Created by XiaoWei on 2016/6/13.
+ */
+public interface CircleMapper extends BaseMapper<Circle> {
+}
--- zyx-services/src/main/java/com/zyx/service/pg/impl/CircleServiceImpl.java
+++ zyx-services/src/main/java/com/zyx/service/pg/impl/CircleServiceImpl.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Service;
 /**
  * Created by XiaoWei on 2016/6/13.
  */
-//@Service("circleService")
+@Service("circleService")
 public class CircleServiceImpl  extends BaseServiceImpl<Circle> implements CircleService{
 }
