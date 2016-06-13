61f2a663f5180550a9d43acf9de2db5be2d6a8b0
邓清海 <396187665@qq.com>
2016-06-13 12:25:28 +0800

    【modify】 pom 配置文件提交

--- zyx-services/src/main/java/com/zyx/service/live/impl/TextLiveItemServiceImpl.java
+++ zyx-services/src/main/java/com/zyx/service/live/impl/TextLiveItemServiceImpl.java
@@ -1,12 +1,10 @@
 package com.zyx.service.live.impl;
 
-import com.zyx.service.live.TextLiveItemService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.zyx.dao.live.TextLiveItemDao;
 import com.zyx.entity.live.TextLiveItem;
 import com.zyx.service.impl.BaseServiceImpl;
+import com.zyx.service.live.TextLiveItemService;
 
 @Service("textLiveItemService")
 public class TextLiveItemServiceImpl extends BaseServiceImpl<TextLiveItem> implements TextLiveItemService {
