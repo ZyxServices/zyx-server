7039927d8c98ba11c3a5f15b13397984ef0b67d3
XiaoWei <xiaoweiqb@126.com>
2016-06-13 13:01:34 +0800

    [modify] 修改 mapper包中xml文件夹xml文件的namespace 为对应service

--- zyx-services/src/main/java/com/zyx/impl/TextLiveItemServiceImpl.java
+++ zyx-services/src/main/java/com/zyx/impl/TextLiveItemServiceImpl.java
@@ -4,6 +4,6 @@ import com.zyx.entity.live.TextLiveItem;
 import com.zyx.service.live.TextLiveItemService;
 import org.springframework.stereotype.Service;
 
-//@Service("textLiveItemService")
+@Service("textLiveItemService")
 public class TextLiveItemServiceImpl extends BaseServiceImpl<TextLiveItem> implements TextLiveItemService {
 }
