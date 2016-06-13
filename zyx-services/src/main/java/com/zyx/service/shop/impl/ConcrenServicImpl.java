5da329ed8baf38f0c87f02754787cdfbc5e0117a
邓清海 <396187665@qq.com>
2016-06-13 11:39:28 +0800

    [add] 商城实现类恢复

---
+++ zyx-services/src/main/java/com/zyx/service/shop/impl/ConcrenServicImpl.java
@@ -0,0 +1,20 @@
+package com.zyx.service.shop.impl;
+
+
+import org.springframework.stereotype.Service;
+
+import com.zyx.entity.playground.Concern;
+import com.zyx.service.impl.BaseServiceImpl;
+import com.zyx.service.shop.ConcrenService;
+
+/**
+ * Created by XiaoWei on 2016/6/7.
+ */
+@Service("concrenService")
+public class ConcrenServicImpl extends BaseServiceImpl<Concern> implements ConcrenService {
+
+    @Override
+    public void custom() {
+
+    }
+}
