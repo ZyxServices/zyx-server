089f78f7ecb8772dc7b1b45cc4b3091dbb103e73
邓清海 <396187665@qq.com>
2016-06-13 11:43:52 +0800

    [modify] live shop Dao改Mapper移动位置

---
+++ zyx-mapper/src/main/java/com/zyx/mapper/live/TextLiveItemMapper.java
@@ -0,0 +1,8 @@
+package com.zyx.mapper.live;
+
+import com.zyx.entity.live.TextLiveItem;
+import com.zyx.mapper.MyMapper;
+
+public interface TextLiveItemMapper extends MyMapper<TextLiveItem>{
+
+}
---
+++ zyx-mapper/src/main/java/com/zyx/mapper/shop/ConcrenMapper.java
@@ -0,0 +1,10 @@
+package com.zyx.mapper.shop;
+
+import com.zyx.entity.playground.Concern;
+import com.zyx.mapper.MyMapper;
+
+/**
+ * Created by xw on 16-6-12.
+ */
+public interface ConcrenMapper extends MyMapper<Concern> {
+}
