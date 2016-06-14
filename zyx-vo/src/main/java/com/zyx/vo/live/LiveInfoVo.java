e228ec10c854275f282a7b15c9b18ddd4bdd786a
邓清海 <396187665@qq.com>
2016-06-14 10:45:09 +0800

    【modify】数据库名变更  VO提交

---
+++ zyx-vo/src/main/java/com/zyx/vo/live/LiveInfoVo.java
@@ -0,0 +1,24 @@
+package com.zyx.vo.live;
+
+public class LiveInfoVo {
+	private Boolean distinct;
+
+	private String oredCriteria;
+	public Boolean getDistinct() {
+		return distinct;
+	}
+
+	public void setDistinct(Boolean distinct) {
+		this.distinct = distinct;
+	}
+
+	public String getOredCriteria() {
+		return oredCriteria;
+	}
+
+	public void setOredCriteria(String oredCriteria) {
+		this.oredCriteria = oredCriteria;
+	}
+	
+
+}
