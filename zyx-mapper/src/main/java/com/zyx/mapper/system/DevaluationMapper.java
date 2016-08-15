8fc450a3181a5bbaee7515252a23963f7bb9e242
MrDeng <396187665@qq.com>
2016-08-15 11:33:15 +0800

    【move modify】 去掉各单独模块中首推内容  直播创建状态默认修改

---
+++ zyx-mapper/src/main/java/com/zyx/mapper/system/DevaluationMapper.java
@@ -0,0 +1,52 @@
+package com.zyx.mapper.system;
+
+import com.zyx.entity.system.Devaluation;
+import com.zyx.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by SubDong on 16-6-28.
+ *
+ * @author SubDong
+ * @version V1.0
+ *          Copyright (c)2016 tyj-版权所有
+ * @title DevaluationMapper
+ * @package com.zyx.mapper.activity
+ * @update 16-6-28 下午3:13
+ */
+@Repository("devaluationMapper")
+public interface DevaluationMapper extends BaseMapper<Devaluation> {
+    /**
+     * 删除首推项
+     *
+     * @param devaluation
+     */
+    int deleteDevaluation(Devaluation devaluation);
+
+    /**
+     * 查询首推项
+     *
+     * @param types
+     * @return
+     */
+    List<Devaluation> queryDevaluation(Integer types);
+
+    /**
+     * 查询相关类型的所有首推ids
+     *
+     * @param types
+     * @return
+     */
+    List<Integer> queryDevaIds(Integer types);
+
+    /**
+     * 根据type和首推现id查询首推信息
+     *
+     * @param types
+     * @param devaluationId
+     * @return
+     */
+    Devaluation queryDevaluationByDevaId(Integer types, Integer devaluationId);
+}
