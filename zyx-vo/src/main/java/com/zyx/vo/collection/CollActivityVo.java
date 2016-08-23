a151c05cc775b9bc9914a91807b684c3a05ccbe9
MrDeng <396187665@qq.com>
2016-08-23 18:14:18 +0800

    【add】 收藏活动

--- zyx-vo/src/main/java/com/zyx/vo/collection/CollActivityVo.java
+++ zyx-vo/src/main/java/com/zyx/vo/collection/CollActivityVo.java
@@ -1,8 +1,19 @@
 package com.zyx.vo.collection;
 
+import com.zyx.vo.activity.ActivityVo;
+
 /**
  * Created by MrDeng on 2016/8/23.
  */
 public class CollActivityVo extends CollectionVo{
 
+    ActivityVo activityInfoVo;
+
+    public ActivityVo getActivityInfoVo() {
+        return activityInfoVo;
+    }
+
+    public void setActivityInfoVo(ActivityVo activityInfoVo) {
+        this.activityInfoVo = activityInfoVo;
+    }
 }
