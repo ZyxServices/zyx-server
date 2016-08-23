846b88080c41d916b400c65d0129e1f330ec7b1d
MrDeng <396187665@qq.com>
2016-08-23 18:47:43 +0800

    【modiyf】 添加活动 收藏查询

--- zyx-mapper/src/main/java/com/zyx/mapper/pg/CircleItemMapper.java
+++ zyx-mapper/src/main/java/com/zyx/mapper/pg/CircleItemMapper.java
@@ -35,5 +35,6 @@ public interface CircleItemMapper extends BaseMapper<CircleItem> {
 
     List<CircleItemVo> getTjCircleItem(@Param("start") Integer start, @Param("end") Integer end);
 
+    CircleItemVo selectCircleItemById(@Param(value = "circleItemId") Integer circleItemId);
 
 }
---
+++ zyx-vo/src/main/java/com/zyx/vo/collection/CollCircleItemVo.java
@@ -0,0 +1,18 @@
+package com.zyx.vo.collection;
+
+import com.zyx.vo.pg.CircleItemVo;
+
+/**
+ * Created by MrDeng on 2016/8/23.
+ */
+public class CollCircleItemVo extends CollectionVo{
+    CircleItemVo circleItemVo;
+
+    public CircleItemVo getCircleItemVo() {
+        return circleItemVo;
+    }
+
+    public void setCircleItemVo(CircleItemVo circleItemVo) {
+        this.circleItemVo = circleItemVo;
+    }
+}
