ae1b599d89356944d551677347645837d8ee47d2
XiaoWei <xiaoweiqb@126.com>
2016-08-31 10:44:57 +0800

    [modify] 动态取消h5拼接，帖子添加单张图片h5拼接

--- zyx-services/src/main/java/com/zyx/service/pg/impl/CircleItemServiceImpl.java
+++ zyx-services/src/main/java/com/zyx/service/pg/impl/CircleItemServiceImpl.java
@@ -70,9 +70,13 @@ public class CircleItemServiceImpl extends BaseServiceImpl<CircleItem> implement
                     StringBuilder sb = new StringBuilder();
                     String[] imgOne = circleItem.getImgUrl().split(",");
                     for (int i = 0; i < imgOne.length; i++) {
-                        sb.append("<img src='http://image.tiyujia.com/" + imgOne[i] + "'></img>");
+                        sb.append("<img src='http://image.tiyujia.com/" + imgOne[i] + "'></img><p>&nbsp;</p>");
                     }
-                    circleItem.setContent(circleItem.getContent() + "</br>" + sb.toString());
+                    circleItem.setContent(circleItem.getContent() + sb.toString());
+                } else {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("<img src='http://image.tiyujia.com/" + circleItem.getImgUrl() + "'></img>");
+                    circleItem.setContent(circleItem.getContent() + sb.toString());
                 }
             }
             save(circleItem);
