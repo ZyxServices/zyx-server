98729ccc641499b49bac8bfab97f480799125df2
MrDeng <396187665@qq.com>
2016-09-01 17:50:55 +0800

    【modify】 直播弹幕 点赞数

--- zyx-services/src/main/java/com/zyx/service/live/impl/BarrageServiceImpl.java
+++ zyx-services/src/main/java/com/zyx/service/live/impl/BarrageServiceImpl.java
@@ -3,6 +3,7 @@ package com.zyx.service.live.impl;
 import java.util.List;
 
 import com.zyx.param.live.BarrageParam;
+import com.zyx.vo.live.BarrageVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,6 @@ import com.zyx.entity.live.Barrage;
 import com.zyx.mapper.live.BarrageMapper;
 import com.zyx.service.BaseServiceImpl;
 import com.zyx.service.live.BarrageService;
-import com.zyx.vo.live.BarrageVo;
 
 @Service("barrageService")
 public class BarrageServiceImpl extends BaseServiceImpl<Barrage> implements BarrageService {
@@ -22,7 +22,7 @@ public class BarrageServiceImpl extends BaseServiceImpl<Barrage> implements Barr
 	}
 
 	@Override
-	public List<Barrage> getList(BarrageParam barrageParam) {
+	public List<BarrageVo> getList(BarrageParam barrageParam) {
 		if(barrageParam.getPager()!=null&&!barrageParam.getPager().verifyPager())
 			barrageParam.setPager(null);
 		return barrageMapper.selectList(barrageParam);
