959ca2282d9610a6027347dde5e6eb3b3fd52bf9
RAINBOW-SUBDONG\Rainbow <shuzichao@126.com>
2016-09-07 16:10:03 +0800

    [fix]

--- zyx-services/src/main/java/com/zyx/service/activity/impl/PageViwesServiceImpl.java
+++ zyx-services/src/main/java/com/zyx/service/activity/impl/PageViwesServiceImpl.java
@@ -95,11 +95,14 @@ public class PageViwesServiceImpl implements PageViwesService {
         if (views != null && sidValue != null) {
             pageViews.setPageviews((views.getPageviews() == null ? 0 : views.getPageviews()) + Integer.valueOf(sidValue));
         } else {
-            if (sidValue != null) {
-                pageViews.setPageviews(Integer.valueOf(sidValue) != null ? Integer.valueOf(sidValue) : 0);
+            if (sidValue != null && !sidValue.equals("null") && !sidValue.equals("")) {
+                pageViews.setPageviews(Integer.valueOf(sidValue));
             } else {
                 pageViews.setPageviews(0);
             }
+        }
+        if(pageViews.getPageviews() == null){
+            pageViews.setPageviews(0);
         }
         return pageViews;
     }
