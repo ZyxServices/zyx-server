7606ed2cc2bdcc1c3c426b607e363c62a1e49988
XiaoWei <xiaoweiqb@126.com>
2016-09-09 16:13:47 +0800

    [add] 优化点赞接口，帖子列表显示正确点赞数量

--- zyx-mapper/src/main/java/com/zyx/mapper/pg/ZanMapper.java
+++ zyx-mapper/src/main/java/com/zyx/mapper/pg/ZanMapper.java
@@ -4,6 +4,7 @@ import com.zyx.entity.pg.Zan;
 import com.zyx.vo.pg.ZanCountVo;
 import com.zyx.mapper.BaseMapper;
 import com.zyx.vo.pg.ZanVo;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +18,8 @@ import java.util.List;
 @Repository("zanMapper")
 public interface ZanMapper extends BaseMapper<Zan> {
     List<ZanCountVo> countZanByBodyId(ZanVo vo);
+
+    Integer exist(@Param("bodyId") Integer body_id, @Param("bodyType") Integer body_type, @Param("accountId") Integer account_id);
+
+    Integer cancelZan(@Param("bodyId") Integer body_id, @Param("bodyType") Integer body_type, @Param("accountId") Integer account_id);
 }
